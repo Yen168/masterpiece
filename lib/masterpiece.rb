8b686037d3b4097ea22beb3fbcb2926af713cc10
yen168 <yenan.chen@baruchmail.cuny.edu>
2015-07-17 21:58:22 -0400

    init

--- lib/masterpiece.rb
+++ lib/masterpiece.rb
@@ -1,5 +1,64 @@
 require "masterpiece/version"
 
 module Masterpiece
-  # Your code goes here...
+  
+  class HTML
+		
+		attr_reader :html_code
+
+		def initialize(&block)
+			
+			instance_eval(&block)
+			
+		end
+
+		private
+		
+		def method_missing(name,*args,&block)
+
+			@html_code ||= ''
+			
+			if args.last == nil
+
+				@html_code << %{<#{name}>}
+
+			elsif args.last.class == Hash
+
+				@html_code << %{<#{name} }
+
+				args.last.each {|k,v| @html_code << %{#{k}="#{v}" }}
+
+				args.first.class != Hash ? @html_code << %{>#{args.first}} : @html_code << %{>}
+
+				#ingnore any string other than first one
+
+			elsif args.last == args.first && args.last.class == String
+
+				@html_code << %{<#{name}>#{args.first}}
+
+			else
+
+				@html_code << %{Something Wrong!!!}
+
+			end
+				
+
+			if block_given?
+
+				instance_eval(&block)
+
+			end
+
+			@html_code << %{</#{name}>}
+
+		end
+
+		def comment(arg = nil)
+
+			@html_code << %{<!-- #{arg} -->}
+
+		end
+
+	end
+	
 end
